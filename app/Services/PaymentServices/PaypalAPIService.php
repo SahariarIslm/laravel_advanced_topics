8cb68ea539af26f3624634a0a0856220ff095697
SahariarIslm <dhruv.islam7584@gmail.com>
2026-03-07 23:43:34 +0600

    A PaypalPaymentService is creted and used in a method using dependencty injection

---
+++ app/Services/PaymentServices/PaypalAPIService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\PaymentServices;
+
+class PaypalAPIService
+{
+    public function processPayment()
+    {
+        // Payment logic here
+        return "Payment of $100 processed successfully";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+use App\Services\PaymentServices\PaypalAPIService;
+// service container is a container that manages all the services / depedencies from laravel core
+Route::get('/', function (PaypalAPIService $paypayAPIService) {
+    dump($paypayAPIService->processPayment());
+    dd(app());
 });
